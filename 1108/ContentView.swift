2e90630a0b02dc6051d1565b378f5d47b2ce680e
Jannnn444 <yucianhuang@gmail.com>
2024-11-08 17:08:21 +0800

    Initial Commit

---
+++ 1108/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1108
+//
+//  Created by Hualiteq International on 2024/11/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1108/_108App.swift
@@ -0,0 +1,17 @@
+//
+//  _108App.swift
+//  1108
+//
+//  Created by Hualiteq International on 2024/11/8.
+//
+
+import SwiftUI
+
+@main
+struct _108App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
